1be6a463a88b7d9484227ee95d2936c3b5c3411e
Nonku-29 <122856617+Nonku-29@users.noreply.github.com>
2023-09-23 09:49:05 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-import requests
+"""Python script to export data in the JSON format."""
+from sys import argv
+from requests import get
 import json
 
-def export_employee_todo_json(employee_id):
-    # Make request to API
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
 
-    # Parse response
-    todos = response.json()
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    user = user_response.json()
-    employee_name = user["username"]
+if __name__ == '__main__':
+    apiEndpoint = 'https://jsonplaceholder.typicode.com'
+    apiUser = get(apiEndpoint + '/users/' + argv[1]).json()['username']
+    todos = get(apiEndpoint + '/todos?userId=' + argv[1]).json()
 
-    # Create JSON object
-    tasks = []
+    record, userData, group = {}, {}, []
     for todo in todos:
-        task = {
-            "task": todo["title"],
-            "completed": todo["completed"],
-            "username": employee_name
-        }
-        tasks.append(task)
-    data = {f"{employee_id}": tasks}
+        record['task'] = todo['title']
+        record['completed'] = todo['completed']
+        record['username'] = apiUser
+        group.append(record)
+        record = {}
 
-    # Write to JSON file
-    with open(f"{employee_id}.json", mode="w") as json_file:
-        json.dump(data, json_file)
+    userData[argv[1]] = group
 
-# Example usage
-export_employee_todo_json(1)
+    with open('{}.json'.format(argv[1]), 'w') as file:
+        json.dump(userData, file)
