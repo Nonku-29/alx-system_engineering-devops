08868f1d115c4192e4c5708b5cdcbfcc80844b99
Nonku-29 <khumalononkuleko2@gmail.com>
2023-09-22 22:40:28 -0700

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+def export_employee_todo_json(employee_id):
+    # Make request to API
+    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+
+    # Parse response
+    todos = response.json()
+    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    user = user_response.json()
+    employee_name = user["username"]
+
+    # Create JSON object
+    tasks = []
+    for todo in todos:
+        task = {
+            "task": todo["title"],
+            "completed": todo["completed"],
+            "username": employee_name
+        }
+        tasks.append(task)
+    data = {f"{employee_id}": tasks}
+
+    # Write to JSON file
+    with open(f"{employee_id}.json", mode="w") as json_file:
+        json.dump(data, json_file)
+
+# Example usage
+export_employee_todo_json(1)
